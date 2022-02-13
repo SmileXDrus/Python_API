a32fb8ea40bd453ecbe92e7ec3857c2156da134f
SmileXDrus <vovamuh@yandex.ru>
2022-02-14 02:58:16 +0300

    Ex11: Тест запроса на метод cookie

--- pytests/test_course_exercise.py
+++ pytests/test_course_exercise.py
@@ -2,6 +2,7 @@
 # А затем с помощью assert проверяет, что фраза действительно короче 15 символов.
 
 import pytest
+import requests
 
 
 class TestSomeSimpleTests:
@@ -10,4 +11,15 @@ class TestSomeSimpleTests:
     def test_check_input(self):
         phrase = input("Set a phrase: ")
         assert len(phrase) < 15
+
+    @pytest.mark.second
+    def test_check_cookie(self):
+        response = requests.get('https://playground.learnqa.ru/api/homework_cookie')
+        if response.cookies.values() is not None:
+            print("first way: ", response.cookies.values()[0])
+            dict_1 = response.cookies.get_dict()
+            print(dict_1)
+            if 'HomeWork' in dict_1:
+                print("second way", dict_1["HomeWork"])
+            assert dict_1["HomeWork"] == 'hw_value'
 
