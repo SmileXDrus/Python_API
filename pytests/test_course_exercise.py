7fa22c3efc9b07a41749ca7a5849b567b73caff4
SmileXDrus <vovamuh@yandex.ru>
2022-02-16 20:24:47 +0300

    some_refactoring ex13

--- pytests/test_course_exercise.py
+++ pytests/test_course_exercise.py
@@ -1,7 +1,10 @@
 # collect-only # https://habr.com/ru/post/269759/
+import json
 
 import pytest
 import requests
+from requests import Response
+
 import data_test
 
 
@@ -41,20 +44,50 @@ class TestSomeSimpleTests:
         (data_test.USER_AGENT_2, data_test.Expected_values_2),
         (data_test.USER_AGENT_3, data_test.Expected_values_3),
         (data_test.USER_AGENT_4, data_test.Expected_values_4),
-        (data_test.USER_AGENT_5, data_test.Expected_values_5)]
+        (data_test.USER_AGENT_5, data_test.Expected_values_5)
+    ]
+    args = [
+        'platform',
+        'browser',
+        'device'
+    ]
 
     @pytest.mark.ex13
+    @pytest.mark.parametrize("arg", args)
     @pytest.mark.parametrize("params_t", params)
-    def test_check_header(self, params_t):
+    def test_check_header(self, params_t, arg):
         (user_agent_t, expected_res_t) = params_t
         with_user_agent = requests.get(
             "https://playground.learnqa.ru/ajax/api/user_agent_check",
             headers={"User-Agent": user_agent_t}
         )
-        dict_1 = with_user_agent.json()
-        assert dict_1['platform'] == expected_res_t['platform'], f"For {user_agent_t}, correct platform expected: " \
-                                                                 f"{expected_res_t['platform']} "
-        assert dict_1['browser'] == expected_res_t['browser'], f"For {user_agent_t}, correct browser expected: " \
-                                                               f"{expected_res_t['browser']}"
-        assert dict_1['device'] == expected_res_t['device'], f"For {user_agent_t}, correct device expected: " \
-                                                             f"{expected_res_t['device']}"
+
+        self.assert_json_value_by_name(
+            user_agent_t,
+            with_user_agent,
+            arg,
+            expected_res_t[arg]
+        )
+        self.assert_json_value_by_name(
+            user_agent_t,
+            with_user_agent,
+            arg,
+            expected_res_t[arg]
+        )
+        self.assert_json_value_by_name(
+            user_agent_t,
+            with_user_agent,
+            arg,
+            expected_res_t[arg]
+        )
+
+    @staticmethod
+    def assert_json_value_by_name(user_agent_t, response: Response, name, expected_value):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+        assert name in response_as_dict, f'Cannot find key {name}'
+        assert response_as_dict[name] == expected_value, f"For {user_agent_t}, " \
+                                                         f"browser expected: {expected_value}, " \
+                                                         f"but actual is {response_as_dict[name]} " \
